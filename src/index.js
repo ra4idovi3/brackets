6a41f885ed56245fc8ead5f99f6c123887661777
ra4idovi3 <al.shportak@gmail.com>
2022-03-07 22:11:30 +0500

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0){
+    return false;
+  }
+  let stack = [];
+  let arr = str.split('');
+  for (let i=0; i < str.length; i++){
+    bracketsConfig.map (el => {
+        if(str[i] === el[1] && stack[stack.length-1] == el[0]){
+            stack.pop(str[i]);
+          }else{  
+            if(str[i] === el[0])
+            {
+            stack.push(str[i]);
+                }
+      }
+    })  
+  }
+  return stack.length === 0;
 }
